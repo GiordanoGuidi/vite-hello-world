21be2ab70c520fa4f2691c8349df0a7fd65e176a
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-31 18:05:29 +0100

    added component AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'AppTitle'
+}
+</script>
+
+
+
+<template>
+    <header>
+        <h1>La mia prima app con Vite!</h1>
+    </header>
+</template>
+
+
+
+<style>
+h1 {
+    color: chartreuse
+}
+</style>
